4d14d182292484f38df9d9d229c47925a356e32a
P_Shreyas <ssp1996@gmail.com>
2017-10-12 10:03:45 +0530

    Model Created

---
+++ src/Model.java
@@ -0,0 +1,12 @@
+public class Model {
+
+    private int calculationValue;
+
+    public void addtwonumbers(int fn,int sn){
+        calculationValue=fn+sn;
+    }
+
+    public int getCalculationValue() {
+        return calculationValue;
+    }
+}
